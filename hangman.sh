f080d30def56eed0036bc20d62f1766e67c89c41
Mitch Terpak <strahbehry@gmail.com>
2017-03-15 09:34:24 +0100

    finalized function structure

--- hangman.sh
+++ hangman.sh
@@ -10,35 +10,68 @@ answer=
 hangman_word=
 guessed_letters=()
 
+#Called when game is lost
 game_lose() {
 	echo -e "You've failed too many times, you're hanged, unlucky"
 	echo -e "Enter Y/N to play again"
 }
 
+#Called when game is won
 game_win(){
 
 }
 
+#Generates the word
 generate_word(){
 
 }
 
+#Prints the word
 print_word(){
 
 }
 
+#Prints guessed chars
+print_chars(){
+
+}
+
+#Prints our poor guy
 print_hangman(){
 
 }
 
+#Prints a prompt for input
 print_prompt(){
 
 }
 
+#Prints help
 print_help(){
 
 }
 
+#Prints the welcome message
+print_welcome(){
+
+}
+
+#Check if the input was valid
+validate_user_input(){
+
+}
+
+#Processes the user input
+process_input(){
+
+}
+
+#Clears the console
 clear_console(){
 	printf "\033c"
+}
+
+#The main program
+main(){
+
 }
